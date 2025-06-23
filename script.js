b3084bc5efc560745875bb56c1f2b3b99c69e704
yusufwahab <yusufabdulwhab@gmail.com>
2025-06-23 16:53:27 +0100

    initial Commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const apiKey = "b6fe0eaa4dd4abf916d015021b533804";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const city = document.querySelector(".display-h1")
+const temp = document.querySelector(".temp")
+const humidity = document.querySelector(".humidity")
+const wind = document.querySelector(".wind-speed")
+const condition = document.querySelector(".condition")
+const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".submit-btn")
+const span = document.querySelector(".span-other")
+const weatherIcon  = document.querySelector(".weather-icon");
+const errorMessage = document.querySelector(".error");
+
+const weatherInfo = document.querySelector(".display");
+
+async function checkWeather(location) {
+    const response = await fetch(apiUrl + location + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        errorMessage.style.display = "block"
+        weatherInfo.style.display  = "none"
+    } else {
+        const data = await response.json();
+    console.log(data)
+
+    city.innerHTML = data.name;
+    temp.innerHTML = Math.round(data.main.temp) + "°C";
+    humidity.innerHTML = data.main.humidity + "%"; 
+    wind.innerHTML = data.wind.speed + "km/h";
+    condition.innerHTML  = data.weather[0].main
+
+    if(data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png"
+    } else if (data.weather[0].main == "Clear" ) {
+        weatherIcon.src = "images/clear.png"
+    } else if (data.weather[0].main == "Rain" ) {
+        weatherIcon.src = "images/rain.png"
+    } else if (data.weather[0].main == "Drizzle" ) {
+        weatherIcon.src = "images/drizzle.png"
+    } else if (data.weather[0].main == "Mist" ) {
+        weatherIcon.src = "images/mist.png"
+    } else if (data.weather[0].main == "Snow" ) {
+        weatherIcon.src = "images/snow.png"
+    } else if (data.weather[0].main == "Humidity" ) {
+        weatherIcon.src = "images/humidity.png"
+    } 
+
+    weatherInfo.style.display = "flex"
+    errorMessage.style.display  = "none"
+    }
+    
+}
+ searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value);
+        if (location === "undefined") {
+            `"Weather location unavailable"`
+        }
+    })
+
